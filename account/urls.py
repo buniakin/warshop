36393b1be895d3c6faa00d18a234dc5c2ed6a36e
Alex Buniakin <lexx@MacBook-Pro-Alex.local>
2022-12-03 11:51:42 +0200

    email field added during registration

--- account/urls.py
+++ account/urls.py
@@ -4,6 +4,7 @@ from .views import profile
 
 
 urlpatterns = [
-    path("signup/", views.signup.as_view(), name="signup"),
+    # path("signup/", views.signup.as_view(), name="signup"),
+    path('signup/', views.signup, name='signup'),
     path('profile', profile)
 ]
--- account/views.py
+++ account/views.py
@@ -1,15 +1,30 @@
+from django.contrib.auth import authenticate, login
 from django.contrib.auth.forms import UserCreationForm
-from django.shortcuts import render
+from .forms import SignUpForm
+from django.shortcuts import render, redirect
 from django.urls import reverse_lazy
 from django.views.generic import CreateView
 
 
-# Create your views here.
+def signup(request):
+    if request.method == 'POST':
+        form = SignUpForm(request.POST)
+        if form.is_valid():
+            user = form.save()
+            user.refresh_from_db()
+            # load the profile instance created by the signal
+            user.save()
+            raw_password = form.cleaned_data.get('password1')
 
-class signup(CreateView):
-    form_class = UserCreationForm
-    success_url = reverse_lazy("login")
-    template_name = "registration/signup.html"
+            # login user after signing up
+            user = authenticate(username=user.username, password=raw_password)
+            login(request, user)
+
+            # redirect user to home page
+            return redirect('home')
+    else:
+        form = SignUpForm()
+    return render(request, 'registration/signup.html', {'form': form})
 
 
 def profile(request):
